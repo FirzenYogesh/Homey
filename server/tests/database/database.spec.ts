43c5909ea6716f9a9aae008d0f2b89d1a3525785
Yogesh S <contact@syogesh.dev>
2022-05-20 19:09:30 +0530

    test case

--- server/tests/database/database.spec.ts
+++ server/tests/database/database.spec.ts
@@ -14,6 +14,26 @@ describe("database tests", () => {
 	it("should be initialized", () => {
 		expect(database).not.to.be.null;
 	});
-});
+	after(() => database.destroy());
 
-describe("pages table test")
+	describe("pages table test", () => {
+		it("should have default value", () => {
+			database
+				.table("pages")
+				.where({ name: "/", id: 1 })
+				.select("id", "name")
+				.first()
+				.then((result) => {
+					expect(result).not.to.be.null;
+					it("name is /", () => {
+						expect(result.name).not.to.be.null;
+						expect(result.name).to.equal("/");
+					});
+					it("id is 1", () => {
+						expect(result.id).not.to.be.null;
+						expect(result.id).to.equal(1);
+					});
+				});
+		});
+	});
+});
